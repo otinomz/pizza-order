27dce94461bfd19576c789d30ef04c38c0983cf8
otinomz <awesomeotinomz@gmail.com>
2022-01-20 22:49:51 +0100

    fix: new changes

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -4,22 +4,22 @@ import React from 'react';
 
 const Featured = () => {
     const images = [
-        "/img/featured.png",
-        "/img/featured2.png",
-        "/img/featured3.png"
+        "/img/featured.jpg",
+        "/img/featured2.jpg",
+        "/img/featured3.jpg"
     ]
     
     return (
         <div className={styles.container}>
-            <Image src="/img/arrowl.png" alt="" layout='fill'/>
+            <Image src="/img/arrowl.jpg" alt="" layout='fill'/>
             <div className={styles.wrapper}>
                 <div className={styles.ImgContainer}>
                     {images.map((img, id) => (
-                        <Image key={id} src="/img/featured.png " alt="" layout="fill"/>
+                        <Image key={id} src="/img/featured.jpg " alt="" layout="fill"/>
                     ))}
                 </div>
             </div>
-            <Image src="/img/arrowr.png" alt="" layout="fill" />  
+            <Image src="/img/arrowr.jpg" alt="" layout="fill" />  
         </div> 
     )
 };
