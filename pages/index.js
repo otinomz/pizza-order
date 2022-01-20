6c7afa2c40d73e03e0bfa27fe1d08ffcf58e3610
otinomz <awesomeotinomz@gmail.com>
2022-01-20 20:10:55 +0100

    feat: code cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,9 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Welcome to Pizza Ordering app</h1>
-      </main>
+      homepage
+      <Image src="/img/logo.png" alt="" layout="fill"/>
 
     </div>
   )
