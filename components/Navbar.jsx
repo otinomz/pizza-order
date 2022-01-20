b9b72ef462aa48bae9ac9b7a82d0c5f5b1f67957
otinomz <awesomeotinomz@gmail.com>
2022-01-20 20:31:51 +0100

    feat: styling and adding structure to the navbar compoenents folder

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
 
     return (
-        <div className={}>
+        <div className={styles.container}>
             Navbar
         </div>
     )
