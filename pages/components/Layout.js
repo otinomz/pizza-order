83ba806ca97803220137f30fa7ec0a0043cbd79a
otinomz <awesomeotinomz@gmail.com>
2022-01-20 20:18:15 +0100

    feat: code cleanup

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
-const Layout = () => {
+const Layout = ({children}) => {
     
     return (
         <div>
-      
+            <Navbar />
+                {children}
+            <Footer/>
         </div>
     )
 };
