d18fce2452596b9ac5d2d1e7344ccd38b6b11e0c
otinomz <awesomeotinomz@gmail.com>
2022-01-20 23:26:31 +0100

    bug-fix: code refactoring

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -3,23 +3,28 @@ import styles from "../styles/Featured.module.css";
 import React from 'react';
 
 const Featured = () => {
-    const images = [
-        "/img/featured.jpg",
-        "/img/featured2.jpg",
-        "/img/featured3.jpg"
+    const imagesss = [
+        "/img/featured.png",
+        "/img/featured2.png",
+        "/img/featured3.png"
     ]
     
     return (
         <div className={styles.container}>
+            <div className={styles.arrowContainer}>
+                <Image src="/img/arrowl.png" alt="" layout='fill'/>
+            </div>
             <Image src="/img/arrowl.png" alt="" layout='fill'/>
             <div className={styles.wrapper}>
                 <div className={styles.ImgContainer}>
-                    {images.map((img, id) => (
-                        <Image key={id} src="/img/featured.jpg " alt="" layout="fill"/>
+                    {imagesss.map(( img, i) => (
+                        (<img src="/imgfeatured.png" alt="" />)
                     ))}
                 </div>
             </div>
-            <Image src="/img/arrowr.png" alt="" layout="fill" />  
+            <div className={styles.arrowContainer}>
+                <Image src="/img/arrowr.png" alt="" layout='fill'/>
+            </div>
         </div> 
     )
 };
