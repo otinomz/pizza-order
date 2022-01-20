1b25d95a7177dbab8c15f9bf0657a971ce9ecac4
otinomz <awesomeotinomz@gmail.com>
2022-01-20 20:55:56 +0100

    feat: listitems added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,16 @@ const Navbar = () => {
                 </div>
 
             </div>
-            <div className={styles.item}> center</div>
+            <div className={styles.item}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>Homepage</li>
+                    <li className={styles.listItem}>Products</li>
+                    <li className={styles.listItem}>Menu</li>
+                    <li className={styles.listItem}>Events</li>
+                    <li className={styles.listItem}>Blog</li>
+                    <li className={styles.listItem}>Contact</li>
+                </ul>
+            </div>
             <div className={styles.item}>right</div>
         </div>
     )
