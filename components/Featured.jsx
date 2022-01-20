b5e9edc21895d8dc28b7733f828e5a3894cbec39
otinomz <awesomeotinomz@gmail.com>
2022-01-20 22:30:12 +0100

    feat: creating the featured folder and css module

---
+++ components/Featured.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import styles from "../styles/Featured.module.css";
+import React from 'react';
+
+const Featured = () => {
+    
+    return (
+        <div>
+            Featured
+        </div>
+    )
+};
+
+export default Featured;
