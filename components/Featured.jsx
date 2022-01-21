3265e47b3aa986c067fce0301e06157d98b002c1
otinomz <awesomeotinomz@gmail.com>
2022-01-21 13:32:35 +0100

    feature: code cleanup

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -20,7 +20,17 @@ const Featured = () => {
         }
         
     ]
-    
+
+    const handleArrow = (direction) => {
+        if (direction === "l") {
+            setNumber(number !== 0 ? number-1 : 2)
+        }
+        if (direction === "r") {
+            setNumber(number !== 2 ? number+1 : 0)
+        }
+    }
+    console.log(number)
+
     return (
         <div className={styles.containers}>
             <div className={styles.arrowContainer} style={{left: 0}} onClick={()=> handleArrow("l")}>
@@ -44,7 +54,7 @@ const Featured = () => {
 
             <div className={styles.arrowContainer}
                 style={{ right: 0 }}
-                onClick={() => handleArrow("l")}>
+                onClick={() => handleArrow("r")}>
                 <Image src="/img/arrowr.png" alt="" layout='fill'/>
             </div>
         </div> 
