3ad89c5425f538136baef7866506f2ee23d038ea
otinomz <awesomeotinomz@gmail.com>
2022-01-22 15:38:56 +0100

    feat: code chnages

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,7 +17,21 @@ const Footer = () => {
                 <div className={styles.card}>
                     <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
                     <p className={styles.text}>
-                        37B, RAYFIELD RESORTS
+                        37B, RAYFIELD RESORTS #125
+                        <br /> JOS, 85022
+                        <br/> (243) 81498
+                    </p>
+                    <p className={styles.text}>
+                        27A, RAYFIELD RESORTS #125
+                        <br /> JOS, 85022
+                        <br/> (243) 81498
+                    </p><p className={styles.text}>
+                        37B, RAYFIELD RESORTS #125
+                        <br /> JOS, 85022
+                        <br/> (243) 81498
+                    </p>
+                    <p className={styles.text}>
+                        JENNY KITCh #126
                         <br /> JOS, 85022
                         <br/> (243) 81498
                     </p>
