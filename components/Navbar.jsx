08ae7b484c89da78fd8499a06d05744f5198c18e
otinomz <awesomeotinomz@gmail.com>
2022-01-22 10:47:39 +0100

    feature: code cleanup

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,9 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import styles from "../styles/Navbar.module.css";
 
-
 const Navbar = () => {
-
     return (
         <div className={styles.container}>
             <div className={styles.item}>
