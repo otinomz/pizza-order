18c5ffd08043fa43423f0eb488da0a5b73e89f18
otinomz <awesomeotinomz@gmail.com>
2022-01-22 11:14:15 +0100

    featured: productlist components created

---
+++ components/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "../styles/ProductList.module.css"
+
+const ProductList = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default ProductList;
