b1f5a59d8d0132704e36a8c3bbdfcb03bfebfd09
otinomz <awesomeotinomz@gmail.com>
2022-01-22 15:33:44 +0100

    feat: addinf texxt layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,14 +6,16 @@ const Footer = () => {
     
     return (
         <div className={styles.container}>
-            <div className={styles.item}>
+            <div className={styles.footerItem}>
                 <Image src="/img/bg.png" layout="fill" alt="" />
                 
             </div>
             <div className={styles.item}>
+                <div className={styles.card}>
+                    <h2>WE DID IT, WELL BAKED SLICE OF PIZZA</h2>
+                </div>
                 <div className={styles.card}></div>
                 <div className={styles.card}></div>
-                <div className={styles.item}></div>
             </div>
         </div>
     )
