504aeaf7e4755a248e398852e7611cdb243859fd
otinomz <awesomeotinomz@gmail.com>
2022-01-22 18:38:07 +0100

    feat: code cleanup

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -4,7 +4,13 @@ import styles from "../styles/Cart.module.css"
 const Cart = () => {
     return (
         <div className={styles.container}>
-
+            <div className={styles.left}>
+                <div className={styles.imgContainer}>
+                    <Image src={pizza.img} objectFit="contain" layout="fill" alt=""/>
+                </div>
+            </div>
+            <div className={styles.right}>
+            </div>
         </div>
     )
 };
