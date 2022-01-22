e0c0b73ffc058c1f990eac719d4a7bb014960156
otinomz <awesomeotinomz@gmail.com>
2022-01-22 15:10:20 +0100

    feat: code cleanup

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -6,12 +6,10 @@ const PizzaCard = () => {
     return (
         <div className={styles.container}>
             <Image src="/img/pizza.png" alt="" width="500" height="500" />
-            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+            <h1 className={styles.title}>FLORI SPIN</h1>
             <span className={styles.price}>$19.90</span>
             <p className={styles.description}>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam consequatur, a suscipit voluptate nemo incidunt minima reprehenderit, modi dicta, perspiciatis optio cumque itaque. Eum ipsum dolores veritatis voluptate facere? Voluptatem?
-                Assumenda possimus reiciendis fugiat impedit
-                ernatur accusantium fuga officiis necessitatibusis at iusto
+                natur accusantium fuga officiis necessitatibusis at iusto
             </p>
             
         </div>
