36e9315c977a77d6c24f12f583d31fc9786500ee
otinomz <awesomeotinomz@gmail.com>
2022-01-22 15:29:24 +0100

    feat: code cleanup

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,8 +4,13 @@ import styles from "../styles/Footer.module.css"
 const Footer = () => {
     
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <div className={styles.item}>
+
+            </div>
+            <div className={styles.item}>
+                
+            </div>
         </div>
     )
 };
