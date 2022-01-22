a4ec65af160d0b3a02f8f8a02325434101fc5d68
otinomz <awesomeotinomz@gmail.com>
2022-01-22 15:08:35 +0100

    feat: new chnages

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -5,7 +5,15 @@ import styles from "../styles/PizzaCard.module.css"
 const PizzaCard = () => {
     return (
         <div className={styles.container}>
-             <Image src="/img/pizza.png" alt="" width="500" height="500" />
+            <Image src="/img/pizza.png" alt="" width="500" height="500" />
+            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+            <span className={styles.price}>$19.90</span>
+            <p className={styles.description}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam consequatur, a suscipit voluptate nemo incidunt minima reprehenderit, modi dicta, perspiciatis optio cumque itaque. Eum ipsum dolores veritatis voluptate facere? Voluptatem?
+                Assumenda possimus reiciendis fugiat impedit
+                ernatur accusantium fuga officiis necessitatibusis at iusto
+            </p>
+            
         </div>
     )
 };
