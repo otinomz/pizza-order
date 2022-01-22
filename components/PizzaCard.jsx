cd4a2f9e2ab77c0e44e5faee16765ec98f177444
otinomz <awesomeotinomz@gmail.com>
2022-01-22 15:06:25 +0100

    feat: adding image

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
 import styles from "../styles/PizzaCard.module.css"
 
 const PizzaCard = () => {
     return (
         <div className={styles.container}>
-            Pizza
+             <Image src="/img/pizza.png" alt="" width="500" height="500" />
         </div>
     )
 };
