932783df81961a00af2c8eb569d1c848b54d4aab
otinomz <awesomeotinomz@gmail.com>
2022-01-22 11:18:20 +0100

    feature: setting up the layout

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -4,8 +4,13 @@ import styles from "../styles/ProductList.module.css"
 const ProductList = () => {
 
     return (
-        <div>
-
+        <div className={styles.pizzaContainer}>
+            <h1 className={styles.title}>The BEST PIZZA IN TOWN</h1>
+            <p className={styles.description}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam consequatur, a suscipit voluptate nemo incidunt minima reprehenderit, modi dicta, perspiciatis optio cumque itaque. Eum ipsum dolores veritatis voluptate facere? Voluptatem?
+                Assumenda possimus reiciendis fugiat impedit
+                ernatur accusantium fuga officiis necessitatibusis at iusto
+            </p>
         </div>
     )
 };
