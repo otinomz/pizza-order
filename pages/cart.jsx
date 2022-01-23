bf7a0467e79b543f795c507f2853e0f99c76d779
otinomz <awesomeotinomz@gmail.com>
2022-01-23 17:31:08 +0100

    feat: layout for the right side of the cart page

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -47,6 +47,20 @@ const Cart = () => {
                 </table>
             </div>
             <div className={styles.right}>
+                <div className={styles.wrapper}>
+                    <h2 className={styles.title}>CART TOTAL</h2>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Subtotal:</b>$79.68
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Total:</b>$79.68
+                    </div>
+
+                    <button>CHECKOUT NOW</button>
+                </div>
             </div>
         </div>
     )
