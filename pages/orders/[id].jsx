0794b57bd9dd20285a0522a79b3f95a74fa6d9c3
otinomz <awesomeotinomz@gmail.com>
2022-01-23 20:09:56 +0100

    feat: code cleanup

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from "next/image"
+import styles from "../../styles/Order.modloule.css"
+
+
+const Order = () => {
+    
+    return (
+        <div>
+      
+        </div>
+    )
+};
+
+export default Order;
+  
