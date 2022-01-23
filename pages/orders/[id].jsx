88e1e73d1872c118cdca873f30235f17929f049e
otinomz <awesomeotinomz@gmail.com>
2022-01-23 20:16:12 +0100

    feat: setting up the order template

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Image from "next/image"
-import styles from "../../styles/Order.modloule.css"
+import styles from "../../styles/Order.module.css"
 
 
 const Order = () => {
     
     return (
-        <div>
-      
+        <div className={styles.container}>
+            <div className={styles.left}>
+            
+            </div>
+            <div className={styles.right}>
+                <div className={styles.wrapper}>
+                    <h2 className={styles.title}>CART TOTAL</h2>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Subtotal:</b>$79.68
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Total:</b>$79.68
+                    </div>
+
+                    <button className={styles.button}>CHECKOUT NOW !</button>
+                </div>
+            </div>
         </div>
     )
 };
