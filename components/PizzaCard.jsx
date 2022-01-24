b6a7064c939138795a2386de6e0aabe7a0b4e14b
otinomz <awesomeotinomz@gmail.com>
2022-01-24 10:10:49 +0100

    feat: container resize

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -5,7 +5,7 @@ import styles from "../styles/PizzaCard.module.css"
 const PizzaCard = () => {
     return (
         <div className={styles.container}>
-            <Image src="/img/pizza.png" alt="" width="500" height="500" />
+            <Image src="/img/pizza.png" alt="" width="300" height="300" />
             <h1 className={styles.title}>FLORI SPIN</h1>
             <span className={styles.price}>$19.90</span>
             <p className={styles.description}>
