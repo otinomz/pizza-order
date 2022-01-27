9313682a8ba882e1e43ff76b75c03772b72e19a3
otinomz <awesomeotinomz@gmail.com>
2022-01-27 11:52:49 +0100

    feat: code cleanup

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -5,7 +5,7 @@ import styles from "../styles/PizzaCard.module.css"
 const PizzaCard = () => {
     return (
         <div className={styles.container}>
-            <Image src="/img/pizza.png" alt="" width="300" height="300" />
+            <Image src="/img/pizza.png" alt="" width="200" height="200" />
             <h1 className={styles.title}>FLORI SPIN</h1>
             <span className={styles.price}>$19.90</span>
             <p className={styles.description}>
