6022994792a7445c213e11de7243fc56cb82890b
otinomz <awesomeotinomz@gmail.com>
2022-01-27 13:31:45 +0100

    feat: code cleanup

--- models/Order.js
+++ models/Order.js
@@ -1,38 +1,30 @@
 import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
-    title: {
+    customer: {
         type: String,
         required: true,
         maxLength: 60,
     },
-    description: {
+    address: {
         type: String,
         required: true,
         maxLength: 250,
     },
-    image: {
-        type: String,
-         required: true,
-    },
-    prices: {
-        type:[Number],
+    total: {
+        type: Number,
         required: true,
     },
-    extraOptions: {
-        type: [{
-            text: {
-                type: String,
-                required: true,
-            },
-            price: {
-                type: Number,
-                required: true,
-            }
-        }],
+    status: {
+        type: Number,
+        default: 0
+    },
+    method: {
+        type: Number,
+        required: true,
     },
 },
-    { timestamps: true }
+{ timestamps: true }
 )
 
 export default mongoose.models.Order || mongoose.model("Order", OrderSchema) 
