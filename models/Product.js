8cc551cd9f69bf1b5d91a9da66b59c6d505dc763
otinomz <awesomeotinomz@gmail.com>
2022-01-27 15:37:27 +0100

    feat: new changes

--- models/Product.js
+++ models/Product.js
@@ -1,38 +1,35 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        maxLength: 60,
+      type: String,
+      required: true,
+      maxlength: 60,
     },
-    image: {
-        type: String,
-        required: true,
+    desc: {
+      type: String,
+      required: true,
+      maxlength: 200,
     },
-    description: {
-        type: String,
-        required: true,
-        maxLength: 250,
+    img: {
+      type: String,
+      required: true,
     },
     prices: {
-        type:[Number],
-        required: true,
+      type: [Number],
+      required: true,
     },
-    extraOptions: {
-        type: [{
-            text: {
-                type: String,
-                required: true,
-            },
-            price: {
-                type: Number,
-                required: true,
-            }
-        }],
+    extraOption: {
+      type: [
+        {
+          text: { type: String, required: true },
+          price: { type: Number, required: true },
+        },
+      ],
     },
-},
-    { timestamps: true }
-)
+  },
+  { timestamps: true }
+);
 
-export default mongoose.models.Product || mongoose.model("Product", ProductSchema)
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
