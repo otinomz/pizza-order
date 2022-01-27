09b84657239dc11e183aeb694d39742530450542
otinomz <awesomeotinomz@gmail.com>
2022-01-27 14:32:51 +0100

    feat: creating the post request

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -10,7 +10,8 @@ export default async function handler(req, res) {
     if (method === "GET") { }
     if (method === "POST") {
         try {
-            
+            const product = await Product.create(req.body)
+            res.status(201).json(product) 
         } catch (err) {
             res.status(500).json(err);
         }
