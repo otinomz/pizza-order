98aae361eea3bf5af8edee8df44d35149e145efc
otinomz <awesomeotinomz@gmail.com>
2022-01-27 13:09:34 +0100

    feat: setting up the models

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,18 @@ import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
     title: {
-        
-    }
+        type: String,
+        required: true,
+        maxLength: 60,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 250,
+    },
+    image: {
+        type: String,
+        required: true,
+        maxLength: 60,
+    },
 })
